fa35e7ddb3772c382c350ea007bab8ec83b126d7
git-rahul23 <jr.rahul07@gmail.com>
2020-07-28 14:24:24 +0530

    Updates

---
+++ precache-manifest.1881e943be8599590a85f208445e5369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d708c37510b1febcaf1a732e6858bf37",
+    "url": "/chartapp/index.html"
+  },
+  {
+    "revision": "615d53f9f2bc3f393790",
+    "url": "/chartapp/static/css/main.db954cd5.chunk.css"
+  },
+  {
+    "revision": "cf5865fb532a28dfb31b",
+    "url": "/chartapp/static/js/2.faf7f7a1.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/chartapp/static/js/2.faf7f7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615d53f9f2bc3f393790",
+    "url": "/chartapp/static/js/main.3d420a8b.chunk.js"
+  },
+  {
+    "revision": "acc4f2dc80e162c743f3",
+    "url": "/chartapp/static/js/runtime-main.343c5dad.js"
+  }
+]);
